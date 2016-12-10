79805e98a3cb30fc878847acbf150b0ef538c42b
sevriugin <sevriugin@gmail.com>
2016-12-10 23:14:23 +0300

    add web3

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var Web3 = require('web3');
+var web3 = new Web3();
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.status(200).json({status:"ok", token:'auth.oauth2.1234567890'});
