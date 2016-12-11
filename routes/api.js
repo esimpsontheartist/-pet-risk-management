4fb7f83b75e5ea4a902c2a4dda85d53dfe2d3171
sevriugin <sevriugin@gmail.com>
2016-12-11 12:43:22 +0300

    fix web3 issue

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,7 @@ var lightwallet 		= require('eth-lightwallet');
 var txutils 			= lightwallet.txutils;
 var signing 			= lightwallet.signing;
 var encryption 			= lightwallet.encryption;
+var keyStore            = ightwallet.keystore;
 var HookedWeb3Provider 	= require("hooked-web3-provider");
 
 var password 			= "!ReGa!2016";
@@ -17,31 +18,30 @@ var host	 			= "http://rega53j4n.eastus.cloudapp.azure.com:8545";
 
 /* GET users listing. */
 router.get('/balance', function(req, res, next) {
+    
     console.error('api/balance called');
     
-    lightwallet.keystore.deriveKeyFromPassword(password, function(err, pwDerivedKey) {
+    keyStore.createVault({password: password}, function (err, ks) {
 
-        console.error('api/balance/lightwallet.keystore.deriveKeyFromPassword called');
+        console.error('api/balance/createVault called');
         
-        var keystore = new lightwallet.keystore(seed, pwDerivedKey);
-        
-        keystore.passwordProvider = function (callback) {
-    	    callback(null, password);
-        };
+        ks.keyFromPassword(password, function (err, pwDerivedKey) {
+            if (err) throw err;
+  
+            ks.generateNewAddress(pwDerivedKey, 5);
+            var addr = ks.getAddresses();
 
-        var web3Provider = new HookedWeb3Provider({
-    	    host: host,
-    	    transaction_signer: keystore
+            var web3Provider = new HookedWeb3Provider({
+    	        host: host,
+    	        transaction_signer: ks
+            });
+
+            web3.setProvider(web3Provider);
+            
+            web3.eth.getBalance(addr[0], function(err, balance) {
+  		        res.status(200).json({status:"ok", address: addr[0], balances: balance});
+  	        });
         });
-
-        web3.setProvider(web3Provider);
-
-        keystore.generateNewAddress(pwDerivedKey, 2);
-        var addresses = keystore.getAddresses();
-
-	    web3.eth.getBalance(addresses[0], function(err, balance) {
-  		    res.status(200).json({status:"ok", address: addresses[0], balances: balance});
-  	    });
     });
 });
 
