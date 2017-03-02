f60d138d4b47979720345ac7a4258bb1c5ab8847
sevriugin <sevriugin@gmail.com>
2017-03-02 23:54:22 +0300

    remove vhost

--- app.js
+++ app.js
@@ -8,13 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var api = require('./routes/api');
-var vhost = require( 'vhost' );
 
 var app = express();
-
-//Create the virtual hosts
-var lexiclub = createVirtualHost("www.lexiclub.org", 'Lexi');
-var lexicard = createVirtualHost("www.lexicard.org", 'public');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'Lexi')));
+app.use(express.static(path.join(__dirname, 'Lexi')));
 app.use(lexiclub);
 app.use(lexicard);
 
